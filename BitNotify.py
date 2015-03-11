8617d3166f0e562f3fda86fa121e9b31cc37347d
James Gillard <jamesgillard@live.co.uk>
2015-03-11 18:41:32 +0000

    Bring Github up-to-date

--- BitNotify.py
+++ BitNotify.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 import os,requests,argparse
 from time import sleep
@@ -7,17 +7,22 @@ from twitter import *
 
 #os.system("mode con cols=24 lines=20")
 
+#use the TwitterStream class for stop/start: https://github.com/sixohsix/twitter/tree/master
+
 #NEED SOME TWEETING ON ERROR MESSAGES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--threshold", help="the margin percentage to trigger a DM",
-                    type=int, default=4)
+parser.add_argument("-t", "--threshold", help="the margin percentage to trigger a DM", type=int, default=8)
+
 parser.add_argument("-d", "--debug", help="we got bugs, irradicate bugs with bug spray / 0",
+                    type=int, default=0)
+parser.add_argument("-dev", help="testing new features yo!",
                     type=int, default=0)
                     
 args = parser.parse_args()
 threshold = args.threshold
 debug = args.debug
+dev = args.dev
 
 if (debug): print('threshold: ', threshold)
 
@@ -36,16 +41,22 @@ def twitAuth():
         )
     return t
 
-def getBitstampUSD(rate):
+def getDM(t):
+    msg = t.direct_messages(count=1)
+    return msg[0]['text']
+
+def getBitstampUSD():
     try:
         data = requests.get(URL_bitstamp, timeout = 10).json()
         USD_last = data["last"]
+        if debug: print('Last price   = $' + USD_last)
     except ValueError:
         print('[Error 1001] Bitstamp is having issues, no surprise there...')
         return 1
- 
-    GBP_last = float(USD_last) / rate
-    if debug: print('Last price   = $' + USD_last)
+    return float(USD_last)
+
+def bitstampUSDBGP(USD_last, rate):
+    GBP_last = float(USD_last) / rate    
     if debug: print(u'And in GBP   = £%.2f' % GBP_last)
     return GBP_last
 
@@ -75,7 +86,7 @@ def sendDM(time, info):
     
 def getTime():
     now = datetime.now()
-    time = now.strftime('%d/%m/%Y %H:%M:%S')
+    time = now.strftime('%d/%m %H:%M:%S')
     if debug: print(time)
     return time
 
@@ -84,37 +95,67 @@ rate = getUSDGBP()
 t = twitAuth()
 histList = [0,0,0,0,0,0,0,0,0,0]
 repeatTemp = 0
-while True:          
-    time = getTime()
-    stampGBP = getBitstampUSD(rate)   
-    bittyGBP = getBitty()     
-    percent = bittyDelta(stampGBP, bittyGBP, time)
-    
-    for i in range(0,9):
-        histList[i] = histList[i+1]
-    histList[9] = percent
-    if debug: print(histList)
-    
-    total = 0
-    for i in range(0,10):
-        total += histList[i]
-    avg = total / 10.0
-    if debug: print(avg)
-    if debug: print('repeatTemp: ', repeatTemp)
+sendLoop = True
 
-    if avg < threshold:
-        repeatTemp = 0
+while True:
+    while sendLoop == True:          
+        time = getTime()
+        stampUSD = getBitstampUSD()
+        stampGBP = bitstampUSDBGP(stampUSD, rate)  
+        bittyGBP = getBitty()     
+        percent = bittyDelta(stampGBP, bittyGBP, time)
+        
+        for i in range(0,9):
+            histList[i] = histList[i+1]
+        histList[9] = percent
+        if debug: print(histList)
+        
+        total = 0
+        for i in range(0,10):
+            total += histList[i]
+        avg = total / 10.0
+        if debug: print('10 min avg: %.2f%%' % avg)
+        if debug: print('repeatTemp: ', repeatTemp)
 
-    if avg >= threshold and repeatTemp > 0:
-        repeatTemp -= 1
-    
-    if avg >= threshold and repeatTemp == 0:
-        print("DM sent")
-        sendDM(time, "BL:%.2f BS:%.2f ---> %.2f%%" % (bittyGBP,stampGBP,percent))
-        repeatTemp = repeatTime
-    
-    print('\n')
-    sleep(60)
+        # if avg dips below threshold, resets repeatTemp
+        if avg < threshold:
+            repeatTemp = repeatTime
+
+        # if avg >= threshold and still counting down, take 1 off count
+        if avg >= threshold and repeatTemp > 0:
+            repeatTemp -= 1
+        
+        # if avg >= threshold and count is zero, sendDM and reset repeatTemp
+        if avg >= threshold and repeatTemp == 0:
+            print("DM sent")
+            sendDM(time, "BL:%.2f BS:%.2f (%.2f) -> %.2f%%" % (bittyGBP,stampGBP,stampUSD,avg))
+            repeatTemp = repeatTime
+
+        if debug: print('Snoozing..............')
+        print('\n')
+        sleep(60)
+
+        if(dev):
+            msg = getDM(t);
+            print(msg)
+            if(msg.lower() =="stop"):
+                print("Sleepy time")
+                t.direct_messages.new(user = 'jamesmgillard', text = "We're gonna stop spamming you for a 'lil while then")
+                sendLoop = False
+
+    while sendLoop == False:
+        msg = getDM(t)
+        if msg.lower() == "start":
+            print("Wakey wakey")
+            t.direct_messages.new(user = 'jamesmgillard', text = "Spammyspamspam time!")
+            sendLoop = True
+            histList = [0,0,0,0,0,0,0,0,0,0]
+            repeatTemp = 0
+            break
+        if debug: print('Sleeping..............')
+        print('\n')
+        sleep(60)
+
 
 
 
